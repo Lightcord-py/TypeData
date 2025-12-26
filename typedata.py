1bfce882aa65d4984ce7a2359abaedcb900aed88
Jamesfrench <119378901+git-jamesfrench@users.noreply.github.com>
2025-12-27 00:20:13 +0100

    Added github

--- typedata.py
+++ typedata.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+# https://github.com/Lightcord-py/TypeData
+
 from typing import get_type_hints
 
 class TypeData:
